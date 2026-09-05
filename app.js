3d3644524dd3def4b0f7bc2f8df6463aea30c1b0
Upendra yadav <uy3234@gmail.com>
2026-09-05 23:47:52 +0530

    navbar of header

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express= require("express");
 const app= express();
 const mongoose= require("mongoose");
+const ejsMate= require("ejs-mate");
 
 app.use(express.json());
 const Listing= require("./models/listing.js");
@@ -22,6 +23,8 @@ app.set("view engine","ejs");
 app.set("views", path.join(__dirname,"views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine('ejs', ejsMate);
+app.use(express.static(path.join(__dirname,"public")));
 
 
 //INDEX ROUTE
